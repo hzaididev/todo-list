5b3f8b5ca6eb3c0e741ebd355270577df467128d
hzaididev <hzaididev@gmail.com>
2022-08-18 02:22:10 +0500

    added action analytics in ToDoController
    Added new action ToDoController::analytics, by default it searches for created tasks in last 7 days, it have option to search between two date range  that can be given in query parameters, we can further search within created or completed dates for the tasks

--- app/Http/Controllers/ToDoController.php
+++ app/Http/Controllers/ToDoController.php
@@ -20,6 +20,61 @@ class ToDoController extends Controller
     }
 
     /**
+     * Search and display listing of the resource.
+     *
+     * params:
+     * start (date)
+     * end (date)
+     * field (string)
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function analytics(Request $request)
+    {
+      $tasksCount = $tasksCompleted = 0;
+      $startDate = $endDate = null;
+      $searchField = 'created_at';
+      $field = 'created';
+
+      if ($request->has('start')) {
+        $startDate = $request->query('start');
+      }
+      if ($request->has('end')) {
+        $endDate = $request->query('end');
+      }
+      if ($request->has('field')) {
+        $field = $request->query('field');
+        if ($field == 'created') {
+          $searchField = 'created_at';
+        } elseif ($field == 'completed') {
+          $searchField = 'completed_at';
+        }
+      }
+
+      if (is_null($startDate) && is_null($endDate)) {
+        $startDate = date('Y-m-d', strtotime('-7 days'));
+        $endDate = date('Y-m-d', strtotime("now"));
+      }
+
+      if ($startDate && $endDate) {
+        $tasks = ToDo::whereDate($searchField, '>=', $startDate)
+          ->whereDate($searchField, '<=', $endDate)->get();
+      } elseif (!is_null($startDate) && is_null($endDate)) {
+        $tasks = ToDo::whereDate($searchField, '>=', $startDate)->get();
+      } elseif (is_null($startDate) && !is_null($endDate)) {
+        $tasks = ToDo::whereDate($searchField, '<=', $endDate)->get();
+      }
+
+      if ($tasks) {
+        $tasksCount = $tasks->count();
+        $tasksCompleted = $tasks->where('status','=','1')->count();
+      }
+
+      return view('analytics', compact('tasks','tasksCount', 'tasksCompleted', 'field', 'startDate', 'endDate'));
+    }
+
+    /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
